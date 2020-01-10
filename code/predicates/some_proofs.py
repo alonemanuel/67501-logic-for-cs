b4cd41fd6565b73548f2a9daa5526d00ca5552a0
Alon Emanuel <alon@alonemanuel.com>
2020-01-10 17:10:50 +0200

    Done 11

--- code/predicates/some_proofs.py
+++ code/predicates/some_proofs.py
@@ -6,6 +6,8 @@
 from predicates.syntax import *
 from predicates.proofs import *
 from predicates.prover import *
+
+
 # from predicates.deduction import *
 # from predicates.prenex import equivalence_of
 
@@ -36,6 +38,7 @@ def syllogism_proof(print_as_proof_forms: bool = False) -> Proof:
     step5 = prover.add_mp('Mortal(aristotle)', step4, step3)
     return prover.qed()
 
+
 def syllogism_proof_with_universal_instantiation(print_as_proof_forms: bool =
                                                  False) -> Proof:
     """Using the method `~predicates.prover.Prover.add_universal_instantiation`,
@@ -63,6 +66,7 @@ def syllogism_proof_with_universal_instantiation(print_as_proof_forms: bool =
     step3 = prover.add_assumption('Man(aristotle)')
     step4 = prover.add_mp('Mortal(aristotle)', step3, step2)
     return prover.qed()
+
 
 def syllogism_all_all_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the assumptions:
@@ -96,8 +100,9 @@ def syllogism_all_all_proof(print_as_proof_forms: bool = False) -> Proof:
     step8 = prover.add_ug('Ax[(Greek(x)->Mortal(x))]', step7)
     return prover.qed()
 
+
 def syllogism_all_all_proof_with_tautological_implication(print_as_proof_forms:
-                                                          bool = False) -> \
+bool = False) -> \
         Proof:
     """Using the method
     `~predicates.prover.Prover.add_tautological_implication`, proves from the
@@ -129,6 +134,7 @@ def syllogism_all_all_proof_with_tautological_implication(print_as_proof_forms:
         '(Greek(x)->Mortal(x))', {step2, step4})
     step6 = prover.add_ug('Ax[(Greek(x)->Mortal(x))]', step5)
     return prover.qed()
+
 
 def syllogism_all_exists_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the assumptions:
@@ -165,8 +171,9 @@ def syllogism_all_exists_proof(print_as_proof_forms: bool = False) -> Proof:
         'Ex[Mortal(x)]', {step2, step6, step7})
     return prover.qed()
 
+
 def syllogism_all_exists_proof_with_existential_derivation(print_as_proof_forms:
-                                                           bool = False) -> \
+bool = False) -> \
         Proof:
     """Using the method `~predicates.prover.Prover.add_existential_derivation`,
     proves from the assumptions:
@@ -198,6 +205,7 @@ def syllogism_all_exists_proof_with_existential_derivation(print_as_proof_forms:
     step6 = prover.add_existential_derivation('Ex[Mortal(x)]', step2, step5)
     return prover.qed()
 
+
 def lovers_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the assumptions:
 
@@ -220,6 +228,7 @@ def lovers_proof(print_as_proof_forms: bool = False) -> Proof:
     # Task 10.4
     return prover.qed()
 
+
 def homework_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the assumptions:
 
@@ -241,9 +250,11 @@ def homework_proof(print_as_proof_forms: bool = False) -> Proof:
     # Task 10.5
     return prover.qed()
 
+
 #: The three group axioms
 GROUP_AXIOMS = frozenset({'plus(0,x)=x', 'plus(minus(x),x)=0',
                           'plus(plus(x,y),z)=plus(x,plus(y,z))'})
+
 
 def right_neutral_proof(stop_before_flipped_equality: bool,
                         stop_before_free_instantiation: bool,
@@ -328,7 +339,7 @@ def right_neutral_proof(stop_before_flipped_equality: bool,
     step19 = prover.add_free_instantiation(
         'plus(minus(minus(x)),plus(minus(x),x))='
         'plus(plus(minus(minus(x)),minus(x)),x)', flipped_associativity,
-        {'x': 'minus(minus(x))','y': 'minus(x)','z': 'x'})
+        {'x': 'minus(minus(x))', 'y': 'minus(x)', 'z': 'x'})
     step20 = prover.add_substituted_equality(
         'plus(plus(minus(minus(x)),minus(x)),x)=plus(0,x)', step8, 'plus(_,x)')
     if stop_before_chained_equality:
@@ -338,6 +349,7 @@ def right_neutral_proof(stop_before_flipped_equality: bool,
         [step11, step12, step13, step14, step15, step16, step17, step18, step19,
          step20, zero])
     return prover.qed()
+
 
 def unique_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the group axioms and from the assumption a+c=a
@@ -353,13 +365,63 @@ def unique_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     """
     prover = Prover(GROUP_AXIOMS.union({'plus(a,c)=a'}), print_as_proof_forms)
     # Task 10.10
+
+    zero = prover.add_assumption('plus(0,x)=x')
+    negation = prover.add_assumption('plus(minus(x),x)=0')
+    associativity = prover.add_assumption('plus(plus(x,y),z)=plus(x,plus(y,z))')
+    flipped_zero = prover.add_flipped_equality('x=plus(0,x)', zero)
+    flipped_negation = prover.add_flipped_equality(
+        '0=plus(minus(x),x)', negation)
+    flipped_associativity = prover.add_flipped_equality(
+        'plus(x,plus(y,z))=plus(plus(x,y),z)', associativity)
+
+    f0 = f'plus(a,c)=a'
+    s0 = prover.add_assumption(f0)
+
+    f1 = f'plus(minus(a),plus(a,c))=plus(minus(a),a)'
+    p1 = f'plus(minus(a),_)'
+    s1 = prover.add_substituted_equality(f1, s0, p1)
+
+    f2 = f'plus(minus(a),a)=0'
+    m2 = {'x': 'a'}
+    s2 = prover.add_free_instantiation(f2, negation, m2)
+
+    f3 = f'plus(minus(a),plus(a,c))=0'
+    s3 = prover.add_chained_equality(f3, [s1, s2])
+
+    f4 = f'plus(plus(minus(a),a),c)=plus(0,c)'
+    p4 = f'plus(_,c)'
+    s4 = prover.add_substituted_equality(f4, s2, p4)
+
+    f5 = f'plus(0,c)=c'
+    m5 = {'x': 'c'}
+    s5 = prover.add_free_instantiation(f5, zero, m5)
+
+    f6 = f'plus(plus(minus(a),a),c)=c'
+    s6 = prover.add_chained_equality(f6, [s4, s5])
+
+    f7 = '0=plus(minus(a),plus(a,c))'
+    s7 = prover.add_flipped_equality(f7, s3)
+
+    f8 = 'plus(minus(a),plus(a,c))=plus(plus(minus(a),a),c)'
+    m8 = {'x': 'minus(a)', 'y': 'a', 'z': 'c'}
+    s8 = prover.add_free_instantiation(f8, flipped_associativity, m8)
+
+    f9 = f'0=c'
+    s9 = prover.add_chained_equality(f9, [s7, s8, s6])
+
+    f10 = 'c=0'
+    s10 = prover.add_flipped_equality(f10, s9)
+
     return prover.qed()
+
 
 #: The six field axioms
 FIELD_AXIOMS = frozenset(GROUP_AXIOMS.union(
     {'plus(x,y)=plus(y,x)', 'times(x,1)=x', 'times(x,y)=times(y,x)',
      'times(times(x,y),z)=times(x,times(y,z))', '(~x=0->Ey[times(y,x)=1])',
      'times(x,plus(y,z))=plus(times(x,y),times(x,z))'}))
+
 
 def multiply_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the field axioms that 0*x=0 (``'times(0,x)=0'``).
@@ -374,7 +436,133 @@ def multiply_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     """
     prover = Prover(FIELD_AXIOMS, print_as_proof_forms)
     # Task 10.11
+
+    zero = prover.add_assumption('plus(0,x)=x')
+    negation = prover.add_assumption('plus(minus(x),x)=0')
+    associativity = prover.add_assumption('plus(plus(x,y),z)=plus(x,plus(y,z))')
+    flipped_zero = prover.add_flipped_equality('x=plus(0,x)', zero)
+    flipped_negation = prover.add_flipped_equality(
+        '0=plus(minus(x),x)', negation)
+    flipped_associativity = prover.add_flipped_equality(
+        'plus(x,plus(y,z))=plus(plus(x,y),z)', associativity)
+
+    add_commu = prover.add_assumption('plus(x,y)=plus(y,x)')
+    mult_one = prover.add_assumption('times(x,1)=x')
+    mult_commu = prover.add_assumption('times(x,y)=times(y,x)')
+    mult_assoc = prover.add_assumption('times(times(x,y),z)=times(x,times(y,z))')
+    mult_inv = prover.add_assumption('(~x=0->Ey[times(y,x)=1])')
+    distrib = prover.add_assumption('times(x,plus(y,z))=plus(times(x,y),times(x,z))')
+
+    f_add_commu = 'plus(y,x)=plus(x,y)'
+    f_mult_one = 'x=times(x,1)'
+    f_mult_commu = 'times(y,x)=times(x,y)'
+    f_mult_assoc = 'times(x,times(y,z))=times(times(x,y),z)'
+    f_distrib = 'plus(times(x,y),times(x,z))=times(x,plus(y,z))'
+
+    flipped_add_commu = prover.add_flipped_equality(f_add_commu, add_commu)
+    flipped_mult_one = prover.add_flipped_equality(f_mult_one, mult_one)
+    flipped_mult_commu = prover.add_flipped_equality(f_mult_commu, mult_commu)
+    flipped_mult_assoc = prover.add_flipped_equality(f_mult_assoc, mult_assoc)
+    flipped_distrib = prover.add_flipped_equality(f_distrib, distrib)
+
+    s0 = flipped_mult_one
+
+    f1 = '1=plus(0,1)'
+    m1 = {'x': '1'}
+    s1 = prover.add_free_instantiation(f1, flipped_zero, m1)
+
+    f2 = 'times(x,1)=times(x,plus(0,1))'
+    p2 = 'times(x,_)'
+    s2 = prover.add_substituted_equality(f2, s1, p2)
+
+    f3 = 'times(x,plus(0,1))=plus(times(x,0),times(x,1))'
+    m3 = {'y': '0', 'z': '1'}
+    s3 = prover.add_free_instantiation(f3, distrib, m3)
+
+    f4 = 'x=plus(times(x,0),times(x,1))'
+    s4 = prover.add_chained_equality(f4, [s0, s2, s3])
+
+    s5 = mult_one
+
+    f6 = 'plus(times(x,0),times(x,1))=plus(times(x,0),x)'
+    p6 = 'plus(times(x,0),_)'
+    s6 = prover.add_substituted_equality(f6, s5, p6)
+
+    f7 = 'x=plus(times(x,0),x)'
+    s7 = prover.add_chained_equality(f7, [s4, s6])
+
+    f8 = 'plus(x,minus(x))=plus(plus(times(x,0),x),minus(x))'
+    p8 = 'plus(_,minus(x))'
+    s8 = prover.add_substituted_equality(f8, s7, p8)
+
+    f9 = 'plus(plus(times(x,0),x),minus(x))=plus(times(x,0),plus(x,minus(x)))'
+    m9 = {'x': 'times(x,0)', 'y': 'x', 'z': 'minus(x)'}
+    s9 = prover.add_free_instantiation(f9, associativity, m9)
+
+    f10 = 'plus(x,minus(x))=plus(times(x,0),plus(x,minus(x)))'
+    s10 = prover.add_chained_equality(f10, [s8, s9])
+
+    f10_1 = 'plus(minus(x),x)=plus(x,minus(x))'
+    m10_1 = {'x': 'minus(x)', 'y': 'x'}
+    s10_1 = prover.add_free_instantiation(f10_1, add_commu, m10_1)
+
+    f10_2 = 'plus(minus(x),x)=plus(times(x,0),plus(x,minus(x)))'
+    s10_2 = prover.add_chained_equality(f10_2, [s10_1, s10])
+
+    f11 = 'plus(times(x,0),plus(x,minus(x)))=plus(minus(x),x)'
+    s11 = prover.add_flipped_equality(f11, s10_2)
+
+    f11_1 = 'plus(minus(x),x)=plus(x,minus(x))'
+    m11_1 = {'x': 'minus(x)', 'y': 'x'}
+    s11_1 = prover.add_free_instantiation(f11_1, add_commu, m11_1)
+
+    f11_2 = 'plus(times(x,0),plus(minus(x),x))=plus(times(x,0),plus(x,minus(x)))'
+    p11_2 = 'plus(times(x,0),_)'
+    s11_2 = prover.add_substituted_equality(f11_2, s11_1, p11_2)
+
+    f11_3 = 'plus(times(x,0),plus(minus(x),x))=plus(minus(x),x)'
+    s11_3 = prover.add_chained_equality(f11_3, [s11_2, s11])
+
+    s12 = negation
+
+    f13 = 'plus(times(x,0),plus(minus(x),x))=0'
+    s13 = prover.add_chained_equality(f13, [s11_3, s12])
+
+    s14 = flipped_negation
+
+    f15 = 'plus(times(x,0),0)=plus(times(x,0),plus(minus(x),x))'
+    p15 = 'plus(times(x,0),_)'
+    s15 = prover.add_substituted_equality(f15, s14, p15)
+
+    f16 = 'plus(times(x,0),0)=0'
+    s16 = prover.add_chained_equality(f16, [s15, s13])
+
+    f17 = 'plus(0,times(x,0))=plus(times(x,0),0)'
+    m17 = {'x': '0', 'y': 'times(x,0)'}
+    s17 = prover.add_free_instantiation(f17, add_commu, m17)
+
+    f18 = 'plus(0,times(x,0))=0'
+    s18 = prover.add_chained_equality(f18, [s17, s16])
+
+    f19 = 'plus(0,times(x,0))=times(x,0)'
+    m19 = {'x': 'times(x,0)'}
+    s19 = prover.add_free_instantiation(f19, zero, m19)
+
+    f20 = 'times(x,0)=plus(0,times(x,0))'
+    s20 = prover.add_flipped_equality(f20, s19)
+
+    f21 = 'times(x,0)=0'
+    s21 = prover.add_chained_equality(f21, [s20, s18])
+
+    f22 = 'times(0,x)=times(x,0)'
+    m22 = {'x': '0', 'y': 'x'}
+    s22 = prover.add_free_instantiation(f22, mult_commu, m22)
+
+    f23 = 'times(0,x)=0'
+    s23 = prover.add_chained_equality(f23, [s22, s21])
+
     return prover.qed()
+
 
 #: The induction axiom
 INDUCTION_AXIOM = Schema(
@@ -384,6 +572,7 @@ PEANO_AXIOMS = frozenset({'(s(x)=s(y)->x=y)', '(~x=0->Ey[s(y)=x])', '~s(x)=0',
                           'plus(x,0)=x', 'plus(x,s(y))=s(plus(x,y))',
                           'times(x,0)=0', 'times(x,s(y))=plus(times(x,y),x)',
                           INDUCTION_AXIOM})
+
 
 def peano_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the axioms of Peano arithmetic that 0+x=x
@@ -401,9 +590,11 @@ def peano_zero_proof(print_as_proof_forms: bool = False) -> Proof:
     # Task 10.12
     return prover.qed()
 
+
 #: The axiom schema of (unrestricted) comprehension
 COMPREHENSION_AXIOM = Schema(
     Formula.parse('Ey[Ax[((In(x,y)->R(x))&(R(x)->In(x,y)))]]'), {'R'})
+
 
 def russell_paradox_proof(print_as_proof_forms: bool = False) -> Proof:
     """Proves from the axioms schema of unrestricted comprehension the
@@ -420,6 +611,7 @@ def russell_paradox_proof(print_as_proof_forms: bool = False) -> Proof:
     prover = Prover({COMPREHENSION_AXIOM}, print_as_proof_forms)
     # Task 10.13
     return prover.qed()
+
 
 def not_exists_not_implies_all_proof(formula: Formula, variable: str,
                                      print_as_proof_forms: bool = False) -> \
@@ -442,6 +634,7 @@ def not_exists_not_implies_all_proof(formula: Formula, variable: str,
     assert is_variable(variable)
     # Optional Task 11.4.1
 
+
 def exists_not_implies_not_all_proof(formula: Formula, variable: str,
                                      print_as_proof_forms: bool = False) -> \
         Proof:
@@ -462,6 +655,7 @@ def exists_not_implies_not_all_proof(formula: Formula, variable: str,
     """
     assert is_variable(variable)
     # Optional Task 11.4.2
+
 
 def not_all_iff_exists_not_proof(formula: Formula, variable: str,
                                  print_as_proof_forms: bool = False) -> Proof:
