56082e1cda01b9cab66a5dbb6626aa6984465ebc
Alon Emanuel <alon@alonemanuel.com>
2020-01-18 12:17:21 +0200

    Done 8

--- code/predicates/prenex.py
+++ code/predicates/prenex.py
@@ -250,18 +250,19 @@ def pull_out_quantifications_across_negation(formula: Formula) -> \
     """
     assert is_unary(formula.root)
     # Task 11.6
+    orig_formula = formula
 
     prover = Prover(Prover.AXIOMS.union(ADDITIONAL_QUANTIFICATION_AXIOMS))
-    if not is_quantifier(formula.first.root):
-        result_formula = formula
+    if not is_quantifier(orig_formula.first.root):
+        result_formula = orig_formula
 
-        f0 = equivalence_of(formula, result_formula)
+        f0 = equivalence_of(orig_formula, result_formula)
         s0 = prover.add_tautology(f0)
 
         return result_formula, prover.qed()
 
     else:
-        formula_without_neg = formula.first
+        formula_without_neg = orig_formula.first
         orig_formula_quantifier = formula_without_neg.root
         flipped_formula_quantifier = 'E' if orig_formula_quantifier == 'A' else 'A'
         formula_var = formula_without_neg.variable
@@ -269,14 +270,34 @@ def pull_out_quantifications_across_negation(formula: Formula) -> \
         pulled_out_predicate, pulled_out_proof = pull_out_quantifications_across_negation(negated_predicate)
         result_formula = Formula.parse(f'{flipped_formula_quantifier}{formula_var}[{pulled_out_predicate}]')
 
-        prover.add_proof(pulled_out_proof.conclusion, pulled_out_proof)
+        prev_proof_ln = prover.add_proof(pulled_out_proof.conclusion, pulled_out_proof)
 
-        f0 = equivalence_of(formula, result_formula)
-        axiom_i = 0 if formula_without_neg.root == 'A' else 1
+        neg_pred = Formula.parse(f'~{formula_without_neg.predicate}')
+        flipped_formula_with_neg_pred = Formula.parse(f'{flipped_formula_quantifier}{formula_var}[{neg_pred}]')
+        ante = pulled_out_proof.conclusion
+        cond = equivalence_of(flipped_formula_with_neg_pred, result_formula)
+        f0 = Formula.parse(f'({ante}->{cond})')
+        axiom_i = 14 if flipped_formula_quantifier == 'A' else 15
         axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
-        m0 = {'x': formula_var,
-              'R': formula_without_neg.predicate.substitute({formula_var: Term('_')})}
+        m0 = {'R': neg_pred.substitute({formula_var: Term('_')}),
+              'Q': pulled_out_predicate.substitute({formula_var: Term('_')}),
+              'x': formula_var,
+              'y': formula_var}
         s0 = prover.add_instantiated_assumption(f0, axiom, m0)
+
+        flipped_w_neg_eq_result = cond
+        s_flipped_w_neg_eq_result = prover.add_mp(flipped_w_neg_eq_result, prev_proof_ln, s0)
+
+        orig_eq_intermediate = equivalence_of(orig_formula, flipped_formula_with_neg_pred)
+        axiom_i = 0 if orig_formula_quantifier == 'A' else 1
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m2 = {'x': formula_var,
+              'R': formula_without_neg.predicate.substitute({formula_var: Term('_')})}
+        s_orig_eq_intermediate = prover.add_instantiated_assumption(orig_eq_intermediate, axiom, m2)
+
+        orig_eq_result = equivalence_of(orig_formula, result_formula)
+        s_final = prover.add_tautological_implication(orig_eq_result,
+                                                      {s_flipped_w_neg_eq_result, s_orig_eq_intermediate})
 
         return result_formula, prover.qed()
 
@@ -327,6 +348,80 @@ Formula) -> \
     assert has_uniquely_named_variables(formula)
     assert is_binary(formula.root)
     # Task 11.7.1
+    orig_formula = formula
+
+    prover = Prover(Prover.AXIOMS.union(ADDITIONAL_QUANTIFICATION_AXIOMS))
+    if not is_quantifier(orig_formula.first.root):
+        result_formula = orig_formula
+
+        f0 = equivalence_of(orig_formula, result_formula)
+        s0 = prover.add_tautology(f0)
+
+        return result_formula, prover.qed()
+
+    else:
+        orig_formula_op = orig_formula.root
+        orig_formula_first = orig_formula.first
+        orig_formula_second = orig_formula.second
+
+        predicate_op_second = Formula.parse(f'({orig_formula_first.predicate}{orig_formula_op}{orig_formula_second})')
+        orig_formula_quantifier = orig_formula_first.root
+        flipped_formula_quantifier = 'E' if orig_formula_quantifier == 'A' else 'A'
+        result_formula_quantifier = flipped_formula_quantifier if orig_formula_op == '->' else orig_formula_quantifier
+        formula_var = orig_formula_first.variable
+
+        pulled_out_predicate, pulled_out_proof = pull_out_quantifications_from_left_across_binary_operator(
+            predicate_op_second)
+        result_formula = Formula.parse(f'{result_formula_quantifier}{formula_var}[{pulled_out_predicate}]')
+
+        prev_proof_ln = prover.add_proof(pulled_out_proof.conclusion, pulled_out_proof)
+
+        # neg_pred = Formula.parse(f'~{formula_without_neg.predicate}')
+        quan_w_pred_op_sec = Formula.parse(f'{result_formula_quantifier}{formula_var}[{predicate_op_second}]')
+        ante = pulled_out_proof.conclusion
+        cond = equivalence_of(quan_w_pred_op_sec, result_formula)
+        f0 = Formula.parse(f'({ante}->{cond})')
+        axiom_i = 14 if result_formula_quantifier == 'A' else 15
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m0 = {'R': predicate_op_second.substitute({formula_var: Term('_')}),
+              'Q': pulled_out_predicate.substitute({formula_var: Term('_')}),
+              'x': formula_var,
+              'y': formula_var}
+        s0 = prover.add_instantiated_assumption(f0, axiom, m0)
+
+        quan_w_pred_op_sec_EQ_result = cond
+        s_quan_w_pred_op_sec_EQ_result = prover.add_mp(quan_w_pred_op_sec_EQ_result, prev_proof_ln, s0)
+
+        orig_EQ_quan_w_pred_op_sec = equivalence_of(orig_formula, quan_w_pred_op_sec)
+        axiom_i = _get_axiom_for_binary_and_quantifier_from_left(orig_formula_op, orig_formula_quantifier)
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m2 = {'x': formula_var,
+              'R': orig_formula_first.predicate.substitute({formula_var: Term('_')}),
+              'Q': orig_formula_second}
+        s_orig_EQ_quan_w_pred_op_sec = prover.add_instantiated_assumption(orig_EQ_quan_w_pred_op_sec, axiom, m2)
+
+        orig_eq_result = equivalence_of(orig_formula, result_formula)
+        s_final = prover.add_tautological_implication(orig_eq_result,
+                                                      {s_quan_w_pred_op_sec_EQ_result, s_orig_EQ_quan_w_pred_op_sec})
+
+        return result_formula, prover.qed()
+
+
+def _get_axiom_for_binary_and_quantifier_from_left(binary_op, quantifier):
+    if quantifier == 'A':
+        if binary_op == '&':
+            return 2
+        elif binary_op == '|':
+            return 6
+        elif binary_op == '->':
+            return 10
+    elif quantifier == 'E':
+        if binary_op == '&':
+            return 3
+        elif binary_op == '|':
+            return 7
+        elif binary_op == '->':
+            return 11
 
 
 def pull_out_quantifications_from_right_across_binary_operator(formula:
@@ -376,6 +471,80 @@ Formula) -> \
     assert is_binary(formula.root)
     # Task 11.7.2
 
+    orig_formula = formula
+
+    prover = Prover(Prover.AXIOMS.union(ADDITIONAL_QUANTIFICATION_AXIOMS))
+    if not is_quantifier(orig_formula.second.root):
+        result_formula = orig_formula
+
+        f0 = equivalence_of(orig_formula, result_formula)
+        s0 = prover.add_tautology(f0)
+
+        return result_formula, prover.qed()
+
+    else:
+        orig_formula_op = orig_formula.root
+        orig_formula_first = orig_formula.first
+        orig_formula_second = orig_formula.second
+
+        first_op_predicate = Formula.parse(f'({orig_formula_first}{orig_formula_op}{orig_formula_second.predicate})')
+        orig_formula_quantifier = orig_formula_second.root
+        result_formula_quantifier = orig_formula_quantifier
+        formula_var = orig_formula_second.variable
+
+        pulled_out_predicate, pulled_out_proof = pull_out_quantifications_from_right_across_binary_operator(
+            first_op_predicate)
+        result_formula = Formula.parse(f'{result_formula_quantifier}{formula_var}[{pulled_out_predicate}]')
+
+        prev_proof_ln = prover.add_proof(pulled_out_proof.conclusion, pulled_out_proof)
+
+        quan_w_first_op_pred = Formula.parse(f'{result_formula_quantifier}{formula_var}[{first_op_predicate}]')
+        ante = pulled_out_proof.conclusion
+        cond = equivalence_of(quan_w_first_op_pred, result_formula)
+        f0 = Formula.parse(f'({ante}->{cond})')
+        axiom_i = 14 if result_formula_quantifier == 'A' else 15
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m0 = {'R': first_op_predicate.substitute({formula_var: Term('_')}),
+              'Q': pulled_out_predicate.substitute({formula_var: Term('_')}),
+              'x': formula_var,
+              'y': formula_var}
+        s0 = prover.add_instantiated_assumption(f0, axiom, m0)
+
+        quan_w_first_op_pred_EQ_result = cond
+        s_quan_w_first_op_pred_EQ_result = prover.add_mp(quan_w_first_op_pred_EQ_result, prev_proof_ln, s0)
+
+        orig_EQ_quan_w_first_op_pred = equivalence_of(orig_formula, quan_w_first_op_pred)
+        axiom_i = _get_axiom_for_binary_and_quantifier_from_right(orig_formula_op, orig_formula_quantifier)
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m2 = {'x': formula_var,
+              'R': orig_formula_second.predicate.substitute({formula_var: Term('_')}),
+              'Q': orig_formula_first}
+        s_orig_EQ_quan_w_first_op_pred = prover.add_instantiated_assumption(orig_EQ_quan_w_first_op_pred, axiom, m2)
+
+        orig_eq_result = equivalence_of(orig_formula, result_formula)
+        s_final = prover.add_tautological_implication(orig_eq_result,
+                                                      {s_quan_w_first_op_pred_EQ_result,
+                                                       s_orig_EQ_quan_w_first_op_pred})
+
+        return result_formula, prover.qed()
+
+
+def _get_axiom_for_binary_and_quantifier_from_right(binary_op, quantifier):
+    if quantifier == 'A':
+        if binary_op == '&':
+            return 4
+        elif binary_op == '|':
+            return 8
+        elif binary_op == '->':
+            return 12
+    elif quantifier == 'E':
+        if binary_op == '&':
+            return 5
+        elif binary_op == '|':
+            return 9
+        elif binary_op == '->':
+            return 13
+
 
 def pull_out_quantifications_across_binary_operator(formula: Formula) -> \
         Tuple[Formula, Proof]:
@@ -423,6 +592,58 @@ def pull_out_quantifications_across_binary_operator(formula: Formula) -> \
     assert has_uniquely_named_variables(formula)
     assert is_binary(formula.root)
     # Task 11.8
+    orig_formula = formula
+    prover = Prover(Prover.AXIOMS.union(ADDITIONAL_QUANTIFICATION_AXIOMS))
+
+    pulled_from_left_f, pulled_from_left_p = pull_out_quantifications_from_left_across_binary_operator(orig_formula)
+    pulled_from_left_s = prover.add_proof(pulled_from_left_p.conclusion, pulled_from_left_p)
+
+    inner_pred_of_pulled_from_left, reversed_quans_and_vars = _get_inner_pred_of_quan_formula(pulled_from_left_f)
+    pulled_from_right_f, pulled_from_right_p = pull_out_quantifications_from_right_across_binary_operator(
+        inner_pred_of_pulled_from_left)
+    pulled_from_right_s = prover.add_proof(pulled_from_right_p.conclusion, pulled_from_right_p)
+
+    curr_R = inner_pred_of_pulled_from_left
+    curr_Q = pulled_from_right_f
+    last_proof_ln = pulled_from_right_s
+
+    for curr_quan, curr_var in reversed_quans_and_vars:
+        ante = prover._lines[last_proof_ln].formula
+        quantified_R = Formula.parse(f'{curr_quan}{curr_var}[{curr_R}]')
+        quantified_Q = Formula.parse(f'{curr_quan}{curr_var}[{curr_Q}]')
+        cond = equivalence_of(quantified_R, quantified_Q)
+        f0 = Formula.parse(f'({ante}->{cond})')
+        axiom_i = 14 if curr_quan == 'A' else 15
+        axiom = ADDITIONAL_QUANTIFICATION_AXIOMS[axiom_i]
+        m0 = {'R': curr_R.substitute({curr_var: Term('_')}),
+              'Q': curr_Q.substitute({curr_var: Term('_')}),
+              'x': curr_var,
+              'y': curr_var}
+        s0 = prover.add_instantiated_assumption(f0, axiom, m0)
+
+        f_mp = cond
+        last_proof_ln = prover.add_mp(f_mp, last_proof_ln, s0)
+
+        curr_R, curr_Q = quantified_R, quantified_Q
+
+    result_formula = curr_Q
+    orig_EQ_result = equivalence_of(orig_formula, result_formula)
+    s_final = prover.add_tautological_implication(orig_EQ_result, {pulled_from_left_s, last_proof_ln})
+
+    return result_formula, prover.qed()
+
+
+def _get_inner_pred_of_quan_formula(quantified_formula):
+    if not is_quantifier(quantified_formula.root):
+        return quantified_formula, []
+    list_of_quan_and_var = []
+    list_of_quan_and_var.append((quantified_formula.root, quantified_formula.variable))
+    curr_pred = quantified_formula.predicate
+    while is_quantifier(curr_pred.root):
+        list_of_quan_and_var.append((curr_pred.root, curr_pred.variable))
+        curr_pred = curr_pred.predicate
+    reversed_list_of_quan_and_var = reversed(list_of_quan_and_var)
+    return curr_pred, reversed_list_of_quan_and_var
 
 
 def to_prenex_normal_form_from_uniquely_named_variables(formula: Formula) -> \
